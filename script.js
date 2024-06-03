baaff58d1f47937a6c26c8864be9960ce3ef8429
Henrique Silva <151173750+heyRickDev@users.noreply.github.com>
2024-06-03 14:36:26 -0300

    change result lines and no-required

--- script.js
+++ script.js
@@ -17,12 +17,19 @@ const adicionarRegistro = (event) => {
   let lineQueries = document.querySelector("main")
 
   for (let index = 0; index < arrayCompID.length; index++) {
-    lineQueries.innerHTML += `
+  //   lineQueries.innerHTML += `
+  // <p>
+  // update sesi.nsucartao set nsu='${arrayNsu[index]}', autorizacao='${arrayAuthorization[index]}' where operacao_id='${arrayOperID[index]}'
+  // <br>
+  // <br>
+  // update comprovantemeiosgeral set credenciadora='CIELO', num_autorizacao='${arrayAuthorization[index]}', credenciadora_rede='${arrayCredRed[index]}' where comprovante_id='${arrayCompID[index]}' and detalhe_id= '1'
+  // </p>
+  // `
+  lineQueries.innerHTML += `
   <p>
-  update sesi.nsucartao set nsu='${arrayNsu[index]}', autorizacao='${arrayAuthorization[index]}' where operacao_id='${arrayOperID[index]}'
-  <br>
-  <br>
-  update comprovantemeiosgeral set credenciadora='CIELO', num_autorizacao='${arrayAuthorization[index]}', credenciadora_rede='${arrayCredRed[index]}' where comprovante_id='${arrayCompID[index]}' and detalhe_id= '1'
+    update comprovantemeiosgeral set num_autorizacao='${arrayAuthorization[index]}' where comprovante_id='${arrayCompID[index]}' and detalhe_id= '1'
+    <br>
+    <br>
   </p>
   `
   }
